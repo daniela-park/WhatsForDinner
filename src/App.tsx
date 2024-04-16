2f2e88418983476987c42d42ac1d35b574817f29
daniela-park <daniela-park@outlook.com>
2024-04-16 14:29:35 +0100

    Images location amended

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import { useState } from 'react'
 import dinnerLogo from './assets/Dinner.png'
+import emptyDishIcon from "./assets/dish.png"
+import bibimbapIcon from "./assets/bibimbap.png"
+import cauliflowerIcon from "./assets/cauliflower.png"
+import japchaeIcon from "./assets/japchae.png"
+import pratoFeitoIcon from "./assets/pf.png"
+import moquecaIcon from "./assets/moqueca.png"
+import pastaIcon from "./assets/coco-and-pepe-pasta.png"
+import risottoIcon from "./assets/risotto.png"
+import pizzaIcon from "./assets/pizza.png"
+import lasagnaIcon from "./assets/lasagna.png"
+import aubergineIcon from "./assets/aubergine.png"
+import sushiIcon from "./assets/sushi.png"
+import noodlesIcon from "./assets/noodles.png"
+import riceBowlIcon from "./assets/chilli-sen-carne.png"
+import fritterIcon from "./assets/corn-fritter.png"
+import menemenIcon from "./assets/menemen.png"
+import dumplingsIcon from "./assets/dumplings.png"
+import burgerIcon from "./assets/burger.png"
+import happyMealIcon from "./assets/mcdonalds.png"
 import './App.css'
 
 function App() {
@@ -8,97 +27,97 @@ function App() {
   const dishes = [
     {
       dishName: "...",
-      dishImg: "./assets/dish.png",
+      dishImg: emptyDishIcon,
       imgAlt: "empty dish icon"
     },
     {
       dishName: "bibimbap",
-      dishImg: "./assets/bibimbap.png",
+      dishImg: bibimbapIcon,
       imgAlt: "bibimbap icon"
     },
     {
       dishName: "cauliflower gochujang",
-      dishImg: "./assets/cauliflower.png",
+      dishImg: cauliflowerIcon,
       imgAlt: "cauliflower icon"
     },
     {
       dishName: "japchae",
-      dishImg: "./assets/japchae.png",
+      dishImg: japchaeIcon,
       imgAlt: "japchae icon"
     },
     {
       dishName: "prato feito",
-      dishImg: "./assets/pf.png",
+      dishImg: pratoFeitoIcon,
       imgAlt: "prato feito icon"
     },
     {
       dishName: "heart of palm moqueca",
-      dishImg: "./assets/moqueca.png",
+      dishImg: moquecaIcon,
       imgAlt: "moqueca icon"
     },
     {
       dishName: "coco & pepe pasta",
-      dishImg: "./assets/coco-and-pepe-pasta.png",
+      dishImg: pastaIcon,
       imgAlt: "pasta icon"
     },
     {
       dishName: "creamy mushroom risotto",
-      dishImg: "./assets/risotto.png",
+      dishImg: risottoIcon,
       imgAlt: "risotto icon"
     },
     {
       dishName: "pizza",
-      dishImg: "./assets/pizza.png",
+      dishImg: pizzaIcon,
       imgAlt: "pizza icon"
     },
     {
       dishName: "vegetable lasagna",
-      dishImg: "./assets/lasagna.png",
+      dishImg: lasagnaIcon,
       imgAlt: "lasagna icon"
     },
     {
       dishName: "miso aubergine",
-      dishImg: "./assets/aubergine.png",
+      dishImg: aubergineIcon,
       imgAlt: "aubergine icon"
     },
     {
       dishName: "sushi",
-      dishImg: "./assets/sushi.png",
+      dishImg: sushiIcon,
       imgAlt: "sushi icon"
     },
     {
       dishName: "soupy noodle",
-      dishImg: "./assets/noodles.png",
+      dishImg: noodlesIcon,
       imgAlt: "noodles icon"
     },
     {
       dishName: "chilli sen carne",
-      dishImg: "./assets/chilli-sen-carne.png",
+      dishImg: riceBowlIcon,
       imgAlt: "rice bowl icon"
     },
     {
       dishName: "corn fritter",
-      dishImg: "./assets/corn-fritter.png",
+      dishImg: fritterIcon,
       imgAlt: "fritter icon"
     },
     {
       dishName: "menemen",
-      dishImg: "./assets/menemen.png",
+      dishImg: menemenIcon,
       imgAlt: "meneme icon"
     },
     {
       dishName: "dumplings",
-      dishImg: "./assets/dumplings.png",
+      dishImg: dumplingsIcon,
       imgAlt: "dumplings icon"
     },
     {
       dishName: "burger",
-      dishImg: "./assets/burger.png",
+      dishImg: burgerIcon,
       imgAlt: "burger icon"
     },
     {
       dishName: "McD",
-      dishImg: "./assets/mcdonalds.png",
+      dishImg: happyMealIcon,
       imgAlt: "Happy meal icon"
     },
   ];
